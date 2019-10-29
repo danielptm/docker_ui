e9231d3f91ff0e93d4f8a0f99c39ec2f59a8c5d3
Daniel <daniel.tuttle@nike.com>
2019-10-29 13:04:53 -0700

    made some small changes

--- ui/src/App.js
+++ ui/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit hizz<code>src/App.js</code> and save to reload.
+          Edit hizzz<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
